da9d3c7e7617258b9977bba82395d023f994b674
preethikasasikumar <preethiiii001@gmail.com>
2025-02-11 21:36:26 -0800

    Add files via upload

---
+++ bytedatatype.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    byte a =100 ,b = 98 ;
+        byte c = (byte) (a + b) ;
+        System.out.println(c);
+        byte d = (byte) (a - b);
+        
+		System.out.println(d);
+	}
+}
