4c6d4b703944850146ea47121bc4484f4431bf3b
preethikasasikumar <preethiiii001@gmail.com>
2025-02-11 22:25:02 -0800

    Create operators1.java

---
+++ operators1.java
@@ -0,0 +1,12 @@
+public class operators
+{
+	public static void main(String[] args) {
+		int a= 44, b = 88, c = 78;
+		System.out.println(a>b && a>c);
+		System.out.println(b>a && b>c);
+		System.out.println(c>a && c>b);
+		System.out.println(a>b || a>c);
+		System.out.println(b>a || b>c);
+		System.out.println(c>a || c>b);
+	}
+}
