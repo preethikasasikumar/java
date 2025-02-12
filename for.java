6f0ff665fd860e4e203b5dc6380a948f5cd046fd
preethikasasikumar <preethiiii001@gmail.com>
2025-02-12 02:53:02 -0800

    Add files via upload

---
+++ for.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+        int a = 7;
+        for( ; ; ){
+            if(a>3){
+                System.out.println(a);
+            }
+            else
+            {
+                break;
+                
+            }
+            a--;
+        }
+    }
+}
