5b2840973a246a6d42f1061eb6a86b49b293fc2d
preethikasasikumar <preethiiii001@gmail.com>
2025-02-11 21:43:17 -0800

    Update bytedatatype.java

--- bytedatatype.java
+++ bytedatatype.java
@@ -1,13 +1,19 @@
 
-
-public class Main
+public class bytedatatype
 {
 	public static void main(String[] args) {
 	    byte a =100 ,b = 98 ;
+	    System.out.println("addition");
         byte c = (byte) (a + b) ;
         System.out.println(c);
+        System.out.println("subtraction");
         byte d = (byte) (a - b);
-        
-		System.out.println(d);
+        System.out.println(d);
+        System.out.println("multiplication");
+        byte e = (byte) (a * b);
+        System.out.println(e);
+         System.out.println("division");
+        byte f = (byte) (a / b) ;
+        System.out.println(f);
 	}
 }
