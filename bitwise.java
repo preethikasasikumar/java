09bc136bee1aadabf62ed0981dd2269dd0b9c8f0
preethikasasikumar <preethiiii001@gmail.com>
2025-02-11 23:12:08 -0800

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,14 @@
+//binary conversions using bitwise operators
+public class bitwise
+{
+	public static void main(String[] args) {
+		int a= 498, b = 333;
+		int c = a & b;
+		System.out.println(c);
+		int d = a | b;
+		System.out.println(d);
+		int e = a ^ b;
+		System.out.print(e);
+		
+	}
+}
