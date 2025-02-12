ebe9154094c1af1b2c56b1040a1385d6d15c4f69
preethikasasikumar <preethiiii001@gmail.com>
2025-02-12 01:43:31 -0800

    Add files via upload

---
+++ primenumbers (1).java
@@ -0,0 +1,14 @@
+public class primenumbers {
+    public static void main(String[] args) {
+        int num = 7;
+        int count = 0;
+        for(int i = 1;i<=num;i++){
+            if(num%i==0)
+            count++;
+            
+        }
+        if(count==2){
+            System.out.println(num+" " +"is a prime number");
+        }
+    }
+}
