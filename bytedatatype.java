c46cd0032e5a4774804ea2a5656fedcec195dc58
preethikasasikumar <preethiiii001@gmail.com>
2025-02-11 21:56:02 -0800

    Update bytedatatype.java

--- bytedatatype.java
+++ bytedatatype.java
@@ -2,8 +2,8 @@
 public class bytedatatype
 {
 	public static void main(String[] args) {
-	    byte a =100 ,b = 98 ;
-	    System.out.println("addition");
+	byte a =100 ,b = 98 ;
+	System.out.println("addition");
         byte c = (byte) (a + b) ;
         System.out.println(c);
         System.out.println("subtraction");
