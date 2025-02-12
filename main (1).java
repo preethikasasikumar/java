3236f3c8cbe55b46281ef6cb6971ec3c153df9e2
preethikasasikumar <preethiiii001@gmail.com>
2025-02-12 02:31:41 -0800

    Add files via upload

---
+++ main (1).java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 6;
+        if(false)
+        System.out.print("a"+"b");
+        else{
+            System.out.println("a"+b);
+        }
+    }
+}
